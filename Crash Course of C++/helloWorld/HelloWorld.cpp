6f6c90bd24a04a1567db4d28572cb1b8e41a0b1f
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2024-09-09 20:28:42 -0600

    :)

---
+++ Crash Course of C++/helloWorld/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!2\n";
+    std::cout << "Hello World!3\n";
+
+    return 0;
+}
