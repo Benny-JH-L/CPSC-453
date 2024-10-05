60f9e28a959b37dee6e5f5451d1e0d964facc81e
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2024-10-05 04:08:39 -0600

    ship can rotate and started with not using the vec3.verts in the ship, need to do it with the diamonds too. BUG CURRENTLY  with rotations with mouse input.

--- assignments/a2/453-skeleton-A21/453-skeleton-master/453-skeleton/main.cpp
+++ assignments/a2/453-skeleton-A21/453-skeleton-master/453-skeleton/main.cpp
@@ -40,13 +40,13 @@ struct GameObject
 	GPU_Geometry ggeom;
 	Texture texture;
 
-	glm::vec3 position;
+	glm::vec3 position = glm::vec3(0.f, 0.f, 1.f);	// initial position
 	float theta; // Object's rotation
 	// Alternatively, you could represent rotation via a normalized heading vec:
 	// glm::vec3 heading;
 	float scale; // Or, alternatively, a glm::vec2 scale;
-	glm::mat4 transformationMatrix;
-	glm::mat4 transformationTexMatrix;
+	glm::mat4 transformationMatrix = mat4(1.0f);
+	glm::mat4 transformationTexMatrix;	// not needed (DELETE)
 };
 
 struct GameData
@@ -69,11 +69,15 @@ struct GameData
 	glm::vec3 currMouseLoc;
 };
 
-void rotateAboutObjCenter(GameObject& obj, float angleOfRotation);
+void rotateAboutObjCenter(GameObject& obj, float degreeOfRotation);
 double calcAngle(glm::vec3 initialV3, glm::vec3 finalV3);
 void drawGameObject(ShaderProgram& shader, GameObject& obj);
 void setGpuGeom(GameObject& obj);
+void translateObj(GameObject& obj, double deltaX, double deltaY);
+void uniformScaleObj(GameObject& obj, float scaleVal);
+void scaleObj(GameObject& obj, float scaleX, float scaleY);
 
+// Debug
 void printVec4Pos(glm::vec4 vec, int vecNum);
 void printVec4Pos(glm::vec4 vec);
 
@@ -93,51 +97,53 @@ public:
 		{
 			shader.recompile();
 		}
+		// TEST
 		else if (key == GLFW_KEY_Q && action == GLFW_PRESS)
 		{
 			glm::mat4 identity = glm::mat4(1.0f);	// identity matrix for transformations (4x4)
-			float angle = glm::radians(45.0f * counter);		// angle of rotation (converts degree to radians)
+			float angle = glm::radians(22.5f);		// angle of rotation (converts degree to radians)
 			glm::vec3 axisOfRotation = glm::vec3(0.0f, 0.0f, 1.0f);	// axis of rotation is z-axis, notice how there is a 1.0f at the 'z' pos
 			glm::mat4 rotateMatrix = glm::rotate(identity, angle, axisOfRotation);	// transformation matrix with the 12-degrees of freedom filled out.
-			gameData.ship.transformationMatrix = rotateMatrix;
+			gameData.ship.transformationMatrix = rotateMatrix * gameData.ship.transformationMatrix;
 			drawGameObject(shader, gameData.ship);
 		}
 		else if (key == GLFW_KEY_E && action == GLFW_PRESS)
 		{
-
-			double angle = calcAngle(gameData.previousMouseLoc, gameData.currMouseLoc);
-			cout << "\nAngle calculated (radians) = " << angle << " (degree) = " << angle * (180.f / piApprox) << endl;
-			// for now rotate SHIP as a test.
-			rotateAboutObjCenter(gameData.ship, angle);
+			// for now rotate SHIP as a test. ( this is the opposite code of the above test of rotating 22.5 CCW), this rotates Clock wise 22.5 degrees
+			rotateAboutObjCenter(gameData.ship, -22.5f);
 			drawGameObject(shader, gameData.ship);
 		}
 
-		counter++;
-		std::cout << "counter = " << counter << std::endl;
+		if (action == GLFW_PRESS)
+		{
+			counter++;
+			std::cout << "counter = " << counter << std::endl;
+		}
 		 
 	}
 
+	// THERE IS A PROBLEM WHERE THE SHIP DISAPEARS, -> current theory is: is due to -nand angle (refer to photos)'
+	// and i need to figure a way to go negative rotations, calculated with the mouse input (look at the y values and see if one is bigger than the other... def with y values prolly.
+
+	// need to figure out how to point the ships nose to the mouse pointer (prolly need another variable for it that points somwhere initialy and moves to mouse location)
+	// ^ MAY fix the issues descrbed above...
 	virtual void cursorPosCallback(double xpos, double ypos)
 	{
 		std::cout << "\nMouse pos: (" << xpos << ", " << ypos << ")" << std::endl;
-		gameData.currMouseLoc = glm::vec3(xpos, ypos, 0);
+		gameData.currMouseLoc = glm::vec3(xpos, ypos, 0.f);
 
 		if (gameData.previousMouseLoc != glm::vec3(0.f, 0.f, -1.f))
 		{
 			double angle = calcAngle(gameData.previousMouseLoc, gameData.currMouseLoc);
 			cout << "\nAngle calculated (radians) = " << angle << " (degree) = " << angle * (180.f / piApprox) << endl;
-			// for now rotate SHIP as a test.
+			// for now rotate as a test.
+			angle = angle * (180.f / piApprox);
 			rotateAboutObjCenter(gameData.ship, angle);
 			drawGameObject(shader, gameData.ship);
 		}
 
 		gameData.previousMouseLoc = gameData.currMouseLoc;	// set the previous mouse location
 	}
-
-	//virtual void mouseButtonCallback(int button, int action, int mods)
-	//{
-	//	std::cout << "Hi there :)))" << std::endl;
-	//}
 
 private:
 	ShaderProgram& shader;
@@ -152,24 +158,23 @@ CPU_Geometry shipGeom(float width, float height)
 	float halfHeight = height / 2.0f;
 	CPU_Geometry retGeom;
 	// vertices for the spaceship quad
-	retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth, halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(-halfWidth, -halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(-halfWidth, halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(halfWidth, -halfHeight, 0.f));
+	//retGeom.verts.push_back(glm::vec3(halfWidth, halfHeight, 0.f));
 
 	// For full marks (Part IV), you'll need to use the following vertex coordinates instead.
 	// Then, you'd get the correct scale/translation/rotation by passing in uniforms into
 	// the vertex shader.
-	/*
 	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
 	retGeom.verts.push_back(glm::vec3(-1.f, -1.f, 0.f));
 	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
 	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
 	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
 	retGeom.verts.push_back(glm::vec3(1.f, 1.f, 0.f));
-	*/
+	
 
 	// texture coordinates
 	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
@@ -225,16 +230,16 @@ CPU_Geometry diamondGeom(float width, float height)
 	return retGeom;
 }
 
-// for now transformations will be done like this, until i set up the uniforms and what now.
-void translate(GameObject& obj, float val1, float val2)
-{
-	for (int i = 0; i < obj.cgeom.verts.size(); i++)
-	{
-		glm::vec3& vecToTranslate = obj.cgeom.verts.at(i);
-		vecToTranslate.x = vecToTranslate.x + val1;
-		vecToTranslate.y = vecToTranslate.y + val2;
-	}
-}
+// for now the start transformation will be done like this, until i set up the uniforms and what now.
+//void translate(GameObject& obj, float val1, float val2)
+//{
+//	for (int i = 0; i < obj.cgeom.verts.size(); i++)
+//	{
+//		glm::vec3& vecToTranslate = obj.cgeom.verts.at(i);
+//		vecToTranslate.x = vecToTranslate.x + val1;
+//		vecToTranslate.y = vecToTranslate.y + val2;
+//	}
+//}
 
 void drawGameObject(ShaderProgram& shader, GameObject& obj)
 {
@@ -260,7 +265,7 @@ int main() {
 	Log::debug("Starting main");
 
 	// debug
-	double angle = calcAngle(glm::vec3(9.f, 2.f, 1.f), glm::vec3(2.f, 6.f, 1.f)); // expected value is 59.0362 degrees
+	double angle = calcAngle(glm::vec3(9.f, 2.f, 1.f), glm::vec3(2.f, 6.f, 1.f)); // expected value is 59.0362 degrees (returns in rads)
 	cout << "\nAngle calculated (radians) = " << angle << " (degree) = " << angle * (180.f/piApprox)  << endl;
 
 	// WINDOW
@@ -292,17 +297,19 @@ int main() {
 	d1.cgeom = diamondGeom(0.14f, 0.14f);
 	d2.cgeom = diamondGeom(0.14f, 0.14f);
 	d3.cgeom = diamondGeom(0.14f, 0.14f);
-	// Put the diamonds starting locations (using this method for now)
-	translate(d0, -0.5f, -0.5f);
-	translate(d1, 0.5f, 0.5f);
-	translate(d2, -0.5f, 0.5f);
-	translate(d3, 0.5f, -0.5f);
-	// Set positions
-	ship.position = glm::vec3(0.f, 0.f, 0.f);
-	d0.position = glm::vec3(-0.5f, -0.5f, 0.f);
-	d1.position = glm::vec3(0.5f, 0.5f, 0.f);
-	d2.position = glm::vec3(-0.5f, 0.5f, 0.f);
-	d3.position = glm::vec3(0.5f, -0.5f, 0.f);
+	// Put the diamonds to their starting locations
+	scaleObj(ship, 0.09f, 0.06f);
+	//translateObj(ship, 0.f, 0.f);	// Center of the screen
+	translateObj(d0, -1.f, -1.f);	// bottom left
+	translateObj(d1, 1.f, 1.f);	// top right
+	translateObj(d2, -1.f, 1.f);	// top left
+	translateObj(d3, 1.f, -1.f);	// bottom right
+	// Set positions (already set in the struct
+	//ship.position = glm::vec3(0.f, 0.f, 0.f);
+	//d0.position = glm::vec3(-0.5f, -0.5f, 0.f);
+	//d1.position = glm::vec3(0.5f, 0.5f, 0.f);
+	//d2.position = glm::vec3(-0.5f, 0.5f, 0.f);
+	//d3.position = glm::vec3(0.5f, -0.5f, 0.f);
 
 	// Set gpu geoms
 	//ship.ggeom.setVerts(ship.cgeom.verts);
@@ -312,25 +319,25 @@ int main() {
 	//d0.ggeom.setVerts(d0.cgeom.verts);
 	//d0.ggeom.setTexCoords(d0.cgeom.texCoords);
 	setGpuGeom(d0);
-	setGpuGeom(d1);
-	setGpuGeom(d2);
-	setGpuGeom(d3);
+	//setGpuGeom(d1);
+	//setGpuGeom(d2);
+	//setGpuGeom(d3);
 
-	// Set initial matrices 
-	glm::mat4 identity1 = glm::mat4(1.0f);	// identity matrix for transformations (4x4)
-	float angle1 = glm::radians(90.f);		// angle of rotation (converts degree to radians)
-	glm::vec3 axisOfRotation1 = glm::vec3(0.0f, 0.0f, 1.0f);	// axis of rotation is z-axis, notice how there is a 1.0f at the 'z' pos
-	glm::mat4 noTransform = glm::rotate(identity1, angle1, axisOfRotation1);	// transformation matrix with the 12-degrees of freedom filled out.
-	ship.transformationMatrix = noTransform;
-	ship.transformationTexMatrix = noTransform;
-	d0.transformationMatrix = noTransform;
-	d0.transformationTexMatrix = noTransform;
-	d1.transformationMatrix = noTransform;
-	d1.transformationTexMatrix = noTransform;
-	d2.transformationMatrix = noTransform;
-	d2.transformationTexMatrix = noTransform;
-	d3.transformationMatrix = noTransform;
-	d3.transformationTexMatrix = noTransform;
+	// Set initial matrices (DO NOT DO THIS - DELETE)
+	//glm::mat4 identity1 = glm::mat4(1.0f);	// identity matrix for transformations (4x4)
+	//float angle1 = glm::radians(90.f);		// angle of rotation (converts degree to radians)
+	//glm::vec3 axisOfRotation1 = glm::vec3(0.0f, 0.0f, 1.0f);	// axis of rotation is z-axis, notice how there is a 1.0f at the 'z' pos
+	//glm::mat4 noTransform = glm::rotate(identity1, angle1, axisOfRotation1);	// transformation matrix with the 12-degrees of freedom filled out.
+	//ship.transformationMatrix = noTransform;
+	//ship.transformationTexMatrix = noTransform;
+	//d0.transformationMatrix = noTransform;
+	//d0.transformationTexMatrix = noTransform;
+	//d1.transformationMatrix = noTransform;
+	//d1.transformationTexMatrix = noTransform;
+	//d2.transformationMatrix = noTransform;
+	//d2.transformationTexMatrix = noTransform;
+	//d3.transformationMatrix = noTransform;
+	//d3.transformationTexMatrix = noTransform;
 
 	// RENDER LOOP
 	while (!window.shouldClose()) {
@@ -356,9 +363,9 @@ int main() {
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		//d0.texture.unbind();
 		drawGameObject(shader, d0);
-		drawGameObject(shader, d1);
-		drawGameObject(shader, d2);
-		drawGameObject(shader, d3);
+		//drawGameObject(shader, d1);
+		//drawGameObject(shader, d2);
+		//drawGameObject(shader, d3);
 
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 
@@ -404,7 +411,12 @@ int main() {
 	return 0;
 }
 
-
+/// <summary>
+/// Returns the angle in radians
+/// </summary>
+/// <param name="initialV3"></param>
+/// <param name="finalV3"></param>
+/// <returns></returns>
 double calcAngle(glm::vec3 initialV3, glm::vec3 finalV3)
 {
 	//float dotProuct = (initialV3.x * finalV3.x) + (initialV3.y * finalV3.y) + (initialV3.z * finalV3.z);
@@ -431,13 +443,18 @@ double calcAngle(glm::vec3 initialV3, glm::vec3 finalV3)
 	return angle;
 }
 
-void rotateAboutObjCenter(GameObject& obj, float angleOfRotation)
+/// <summary>
+/// 
+/// </summary>
+/// <param name="obj"></param>
+/// <param name="degreeOfRotation"> in degrees, a float.</param>
+void rotateAboutObjCenter(GameObject& obj, float degreeOfRotation)
 {
 	mat4 translateToOrigin = glm::translate(mat4(1.0f), -obj.position);	// Used to translate all the object's vec3 verts by -Position (Position should be at origin)
 	mat4 translateBack = glm::translate(glm::mat4(1.0f), obj.position);
 
 	glm::mat4 identity = glm::mat4(1.0f);					// identity matrix for transformations (4x4)
-	float angle = glm::radians(angleOfRotation);			// angle of rotation (converts degree to radians)
+	float angle = glm::radians(degreeOfRotation);			// angle of rotation (converts degree to radians)
 	glm::vec3 axisOfRotation = glm::vec3(0.0f, 0.0f, 1.0f);	// axis of rotation is z-axis, notice how there is a 1.0f at the 'z' pos
 
 	glm::mat4 rotateMatrix = glm::rotate(identity, angle, axisOfRotation);	// transformation matrix with the 12-degrees of freedom filled out.
@@ -445,30 +462,51 @@ void rotateAboutObjCenter(GameObject& obj, float angleOfRotation)
 	// 1. translate by -Position (Position should be (0,0,0) after)
 	// 2. do rotation
 	// 3. translate by Position
-	mat4 model = translateBack * rotateMatrix * translateToOrigin;
-	obj.transformationMatrix = model;
-	obj.transformationTexMatrix = model;
+	mat4 model = translateBack * rotateMatrix * translateToOrigin;	// resulting matrix of all those transformations
 
-	// do the transformations on the obj's vec3's
-	for (int i = 0; i < obj.cgeom.verts.size(); i++)
-	{
-		//obj.cgeom.verts.at(i) = model * glm::vec4(obj.cgeom.verts.at(i), 0.f);	// idk if i want to have '0' or '1' for 4-th cord
-		//obj.cgeom.texCoords.at(i) = model * glm::vec4(obj.cgeom.texCoords.at(i), 0.f, 0.f);
-	}
+	// Update transformation matrix
+	obj.transformationMatrix = model * obj.transformationMatrix;	// multiply the newly calculated model with the old model to get the actual model (want to retain the previous transformations)
+	//obj.transformationTexMatrix = model; // (DELETE)
+
+	//// do the transformations on the obj's vec3's (DELETE)
+	//for (int i = 0; i < obj.cgeom.verts.size(); i++)
+	//{
+	//	//obj.cgeom.verts.at(i) = model * glm::vec4(obj.cgeom.verts.at(i), 0.f);	// idk if i want to have '0' or '1' for 4-th cord
+	//	//obj.cgeom.texCoords.at(i) = model * glm::vec4(obj.cgeom.texCoords.at(i), 0.f, 0.f);
+	//}
 }
 
-void translateObj(GameObject& obj, float deltaX, float deltaY)
+void translateObj(GameObject& obj, double deltaX, double deltaY)
 {
 	// Create translation matrix
-	glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(deltaX, deltaY, 0.0f));
+	mat4 translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(deltaX, deltaY, 0.f));
 
 	// Update the object's position
-	obj.position += glm::vec3(deltaX, deltaY, 0.0f);
+	obj.position += vec3(deltaX, deltaY, 0.0f);
 
 	// Update the transformation matrix
-	obj.transformationMatrix = translationMatrix * obj.transformationMatrix; // Apply translation to existing transformations
+	obj.transformationMatrix = translationMatrix + obj.transformationMatrix; // update the transformation matrix
 
+	// DELETE
 	//obj.transformationTexMatrix = translationMatrix * obj.transformationTexMatrix; // Apply translation to texture transformation
+}
+
+void uniformScaleObj(GameObject& obj, float scaleVal)
+{
+	// Create scale matrix
+	mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(scaleVal, scaleVal, 0.f));
+
+	// Update the transformation matrix
+	obj.transformationMatrix = scaleMatrix * obj.transformationMatrix; // update the transformation matrix
+}
+
+void scaleObj(GameObject& obj, float scaleX, float scaleY)
+{
+	// Create scale matrix
+	mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(scaleX, scaleY, 0.f));
+
+	// Update the transformation matrix
+	obj.transformationMatrix *= scaleMatrix; // update the transformation matrix
 }
 
 /// <summary>
